634fbede958453840fc1e5bf301fd5746743664c
useryu1015 <2225469536@qq.com>
2024-01-11 10:45:46 +0800

    update readme

--- 1.alarm_sys/README.md
+++ 1.alarm_sys/README.md
@@ -1,4 +1,4 @@
-# 红外警报系统（毕设）
+## 红外警报系统（毕设）
 > 视频介绍：[红外警报系统](https://www.bilibili.com/video/BV1cT4y1T7gx/?spm_id_from=333.337.search-card.all.click&vd_source=5292a23e9a80c849a2c727ed746aadb7)
 
 > 博文：[矩阵按键控制](https://blog.csdn.net/ling0604/article/details/109643518)
--- generate_readme.sh
+++ generate_readme.sh
@@ -3,8 +3,19 @@ file_name="README.md"
 
 rm -f $file_name
 #find ./ -name $file_name -type f -exec cat {} + >> template.md
+#find ./ -name "$file_name" -type f -exec sh -c 'echo "File: $0" >> template.md; cat "$0" >> template.md; echo "---------------------------" >> template.md' {} \;
 
-find ./ -name "$file_name" -type f -exec sh -c 'echo "File: $0" >> template.md; cat "$0" >> template.md; echo "---------------------------" >> template.md' {} \;
 
+for file in $(find $FILE_DIR -type f -name "$file_name" | sort); do
+    cat $file >> template.md
+    echo FILE: $file
+
+    file_load=$(echo "$file" | sed "s/$file_name//g")
+    echo "> [项目仓库]($file_load)"  >> template.md
+    echo "---------------------------"  >> template.md
+done
 
 mv template.md $file_name
+
+
+
